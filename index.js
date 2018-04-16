58ffa87d6482db66d2aa66b88ed40f3e40ed5249
blondie libra <malevolentninja@gmail.com>
2018-04-16 20:46:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
+
 function shout(string) {
-  return string.toUpperCase();
+  return string.toUpperCase()
 }
 
-function whisper(string){
-  return string.toLowerCase();
+function whisper(string) {
+  return string.toLowerCase()
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase());
+  console.log(string.toUpperCase())
 }
 
-function logWhisper(string){
-  console.log(string.toLowerCase());
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
 
-function sayHiToGrandma(string){
-    string.toUpperCase() === string;
-    return "YES INDEED!";
-}
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
 
-function sayHiToGrandma(string){
-    string.toLowerCase() === string;
-    return "I can't hear you";
-}
-
-function sayHiToGrandma(string){
-    string === "I love you, Grandma";
-    return "I love you, too";
-}
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
